f3673ca912938ca4e2c114ed35d862aff3bdb400
sandeep <sandeepkp601@gmail.com>
2021-11-01 22:54:30 +0530

    sandeep

--- samruddhi/settings.py
+++ samruddhi/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'gksid++a873o^istkr=-)__vqsifoxxofg2#g)4uk6z(2fqglh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
